adb682baa41788de8644c677aaa571dfb574e645
tanmayasamantray <tanmay20042001@gmail.com>
2025-01-03 12:38:17 +0530

    updated till subscription form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './commonComponent/header/header.component';
+import { CategoryNavbarComponent } from "./commonComponent/category-navbar/category-navbar.component";
+import { FooterComponent } from "./commonComponent/footer/footer.component";
+import { PostCardComponent } from "./commonComponent/post-card/post-card.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, CategoryNavbarComponent, FooterComponent, PostCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/commonComponent/header/header.component.ts
+++ src/app/commonComponent/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  brandpath = "https://drive.google.com/file/d/1EU_guDdjLVs2hJ4ZIOA0tsmr-osc9Q8k/view?usp=sharing"  
 }

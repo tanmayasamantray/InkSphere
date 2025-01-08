0f9f866bfc66f94c7f602d28667fcfe2ee7fe7c9
tanmayasamantray <tanmay20042001@gmail.com>
2025-01-08 13:39:22 +0530

    design homepage complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from './commonComponent/header/header.component';
 import { CategoryNavbarComponent } from "./commonComponent/category-navbar/category-navbar.component";
 import { FooterComponent } from "./commonComponent/footer/footer.component";
 import { PostCardComponent } from "./commonComponent/post-card/post-card.component";
+import { SubscriptionFormComponent } from "./subscription-form/subscription-form.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, CategoryNavbarComponent, FooterComponent, PostCardComponent],
+  imports: [RouterOutlet, HeaderComponent, CategoryNavbarComponent, FooterComponent, SubscriptionFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { PostCardComponent } from "../../commonComponent/post-card/post-card.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [PostCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

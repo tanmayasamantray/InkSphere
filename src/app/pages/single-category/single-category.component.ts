e7fe7900aca114ed29caa910b222ce86fb9a9545
tanmayasamantray <tanmay20042001@gmail.com>
2025-01-09 21:30:48 +0530

    refactor: update layout and styles for header, footer, and various components; add comment form and enhance about/terms pages

--- src/app/pages/single-category/single-category.component.ts
+++ src/app/pages/single-category/single-category.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { PostCardComponent } from "../../commonComponent/post-card/post-card.component";
 
 @Component({
   selector: 'app-single-category',
-  imports: [],
+  imports: [PostCardComponent],
   templateUrl: './single-category.component.html',
   styleUrl: './single-category.component.css'
 })
--- src/app/pages/single-post/single-post.component.ts
+++ src/app/pages/single-post/single-post.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { PostCardComponent } from "../../commonComponent/post-card/post-card.component";
+import { CommentFormComponent } from "../../comments/comment-form/comment-form.component";
+import { CommentListComponent } from "../../comments/comment-list/comment-list.component";
 
 @Component({
   selector: 'app-single-post',
-  imports: [],
+  imports: [PostCardComponent, CommentFormComponent, CommentListComponent],
   templateUrl: './single-post.component.html',
   styleUrl: './single-post.component.css'
 })
